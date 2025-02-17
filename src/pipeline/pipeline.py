6cef346ec6b23a40c1e3cb5de43cf1cf95795881
Mateen Khan <mateenmk2428@gmail.com>
2025-02-17 12:09:11 +0530

    src/pipeline/pipeline.py updated

--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -1,5 +1,5 @@
 import sys
-from src.core.logger import get_logger
+from src.core.logger import logging
 from src.core.exception import DocGenException
 from src.components.clone_repo import RepositoryCloner
 from src.components.analyze_repo import RepositoryAnalyzer
@@ -9,11 +9,10 @@ from src.core.entities.artifact_entity import AnalyzeRepoArtifact
 
 class RepositoryPipeline:
     def __init__(self, repo_url):
-        self.logger = get_logger("pipeline")
 
-        self.logger.info("* " * 50)
-        self.logger.info("- - - - - Starting Repository Pipeline - - - - -")
-        self.logger.info("* " * 50)
+        logging.info("* " * 50)
+        logging.info("- - - - - Starting Repository Pipeline - - - - -")
+        logging.info("* " * 50)
         
         self.repo_url = repo_url
         self.clone_repo_config = CloneRepoConfig(repo_url=self.repo_url)
@@ -23,15 +22,15 @@ class RepositoryPipeline:
         This method starts the cloning process for the repository.
         """
         try:
-            self.logger.info("\n$ Entering start_clone_repository method of RepositoryPipeline class:")
+            logging.info("\n$ Entering start_clone_repository method of RepositoryPipeline class:")
             
             clone_repo = RepositoryCloner(self.clone_repo_config)
             clone_repo.clone()
             
-            self.logger.info("- " * 50)
-            self.logger.info("- - - Repository Cloned Successfully! - - -")
-            self.logger.info("! Exiting start_clone_repository method of RepositoryPipeline class:")
-            self.logger.info("_" * 100)
+            logging.info("- " * 50)
+            logging.info("- - - Repository Cloned Successfully! - - -")
+            logging.info("! Exiting start_clone_repository method of RepositoryPipeline class:")
+            logging.info("_" * 100)
         
         except Exception as e:
             raise DocGenException(e, sys) from e
@@ -41,16 +40,16 @@ class RepositoryPipeline:
         This method starts the analysis process for the cloned repository.
         """
         try:
-            self.logger.info("\n$ Entering start_analyze_repository method of RepositoryPipeline class:")
+            logging.info("\n$ Entering start_analyze_repository method of RepositoryPipeline class:")
             
             analyze_config = AnalyzeRepoConfig(repo_path=self.clone_repo_config.repo_path)
             analyze_repo = RepositoryAnalyzer(analyze_config)
             analyze_artifact = analyze_repo.analyze()
             
-            self.logger.info("- " * 50)
-            self.logger.info("- - - Repository Analysis Completed Successfully! - - -")
-            self.logger.info("! Exiting start_analyze_repository method of RepositoryPipeline class:")
-            self.logger.info("_" * 100)
+            logging.info("- " * 50)
+            logging.info("- - - Repository Analysis Completed Successfully! - - -")
+            logging.info("! Exiting start_analyze_repository method of RepositoryPipeline class:")
+            logging.info("_" * 100)
 
             return analyze_artifact
         
@@ -65,7 +64,7 @@ class RepositoryPipeline:
             self.start_clone_repository()
             analyze_artifact = self.start_analyze_repository()
             
-            self.logger.info("Pipeline executed successfully.")
+            logging.info("Pipeline executed successfully.")
             return analyze_artifact
         
         except Exception as e:
